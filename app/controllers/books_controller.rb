b279bc451dedaa9a00dabdcd8b38edb3205da194
Paul Yun <paul.a.yun@gmail.com>
2015-06-26 02:00:18 -0400

    Add/delete book function added.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,37 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    if @book.destroy
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :isbn, :genre)
+  end
 
 end
